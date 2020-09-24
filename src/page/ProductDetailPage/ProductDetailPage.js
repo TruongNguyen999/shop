81fcac5994366dbd6e0a72f293128b9266d97adc
TruongNguyen999 <nguyentruong061999@gmail.com>
2020-09-24 22:41:41 +0700

    branch-3

--- src/page/ProductDetailPage/ProductDetailPage.js
+++ src/page/ProductDetailPage/ProductDetailPage.js
@@ -5,10 +5,14 @@ import MessageProductRalated from '../../components/ConstType/MessageProductRala
 import ProductRalated from '../../components/Content/ProductRalated';
 import ProductRalatedItem from '../../components/Content/ProductRalatedItem';
 import ProductDetailItem from './ProductDetailItem';
+import { Product_Cart_dispatch } from './../../actions/index';
+import { changeMessage_add_dispatch } from './../../actions/mesage';
+import * as message from './../../contants/Message';
+
 
 const ProductDetailPage = props => {
 
-    const { productDetails } = props
+    const { productDetails, add_product, changeMessage } = props
 
     const onShowratiing = ratting => {
         let result = []
@@ -43,13 +47,28 @@ const ProductDetailPage = props => {
         return result
     }
 
+    const onShowconfiguration = info => {
+        let result = null;
+        if (info.length > 0) {
+            result = info.map((inf, index) => {
+                return <li className="list-group-item" key={index}>{inf.name}</li>
+            })
+        }
+        return result;
+    }
+
+    const onAddToCard = product => {
+        add_product(product);
+        changeMessage(message.MESSAGE_ADD)
+    }
+
     if (productDetails.product_discount) {
         return (
             <>
                 <div className="container-fluid padding product-detail">
                     <div className="row padding my-2">
                         <div className="col-md-5 text-center">
-                            <a href>
+                            <a href={productDetails.product_discount.images} alt={productDetails.product_discount.name}>
                                 <img className='img-fluid' width='350px' src={productDetails.product_discount.images} alt={productDetails.product_discount.name} />
                             </a>
                             <div>
@@ -62,17 +81,21 @@ const ProductDetailPage = props => {
                                     </li>
                                 </ul>
                                 <p>Giá: {productDetails.product_discount.price} $</p>
-                                <button className='btn btn-outline-primary'><i className='fa fa-shopping-cart'></i> Thêm Vào Giỏ Hàng</button>
+                                <Link className='btn btn-outline-primary' to='/product-cart' onClick={() => onAddToCard(productDetails)}><i className='fa fa-shopping-cart'></i> Thêm Vào Giỏ Hàng</Link>
+                            </div>
+                            <hr className='my-4' />
+                            <div className='card text-left'>
+                                <h5><i className='text-danger'>Thông tin sản phẩm : </i><span className='text-secondary'>{productDetails.product_discount.details.decscription}</span> </h5>
                             </div>
                         </div>
                         <div className="col-md-4">
-                            <h1>{productDetails.product_discount.name}</h1>
-                            <hr className='my-2' />
-                            <p><i>Thông tin sản phẩm : </i>{productDetails.product_discount.details.decscription} </p>
-                            <hr className='my-2' />
-                            <h4>{productDetails.product_discount.details.configuration}</h4>
-                            <hr className='my-2' />
-                            <p>{productDetails.product_discount.details.specialOffers}</p>
+                            <h3 className='text-center text-danger'>{productDetails.product_discount.name}</h3>
+                            <hr className='my-4' />
+                            <ul className="list-group text-success">
+                                {onShowconfiguration(productDetails.product_discount.details.configuration)}
+                            </ul>
+                            <hr className='my-4' />
+                            <p className='text-primary'>{productDetails.product_discount.details.specialOffers}</p>
                             <hr className='my-4' />
                             <ul className='imageDeatil'>
                                 <li>
@@ -109,7 +132,7 @@ const ProductDetailPage = props => {
                 <div className="container-fluid padding product-detail">
                     <div className="row padding my-2">
                         <div className="col-md-5 text-center">
-                            <a href>
+                            <a href={productDetails.product.images} alt={productDetails.product.name}>
                                 <img className='img-fluid' width='350px' src={productDetails.product.images} alt={productDetails.product.name} />
                             </a>
                             <div>
@@ -122,17 +145,21 @@ const ProductDetailPage = props => {
                                     </li>
                                 </ul>
                                 <p>Giá: {productDetails.product.price} $</p>
-                                <button className='btn btn-outline-primary'><i className='fa fa-shopping-cart'></i> Thêm Vào Giỏ Hàng</button>
+                                <Link className='btn btn-outline-primary' to='/product-cart' onClick={() => onAddToCard(productDetails)}><i className='fa fa-shopping-cart'></i> Thêm Vào Giỏ Hàng</Link>
+                            </div>
+                            <hr className='my-4' />
+                            <div className='card text-left'>
+                                <h5><i className='text-danger'>Thông tin sản phẩm : </i><span className='text-secondary'>{productDetails.product.details.decscription}</span> </h5>
                             </div>
                         </div>
                         <div className="col-md-4">
-                            <h1>{productDetails.product.name}</h1>
-                            <hr className='my-2' />
-                            <p><i>Thông tin sản phẩm : </i>{productDetails.product.details.decscription} </p>
-                            <hr className='my-2' />
-                            <h4>{productDetails.product.details.configuration}</h4>
-                            <hr className='my-2' />
-                            <p>{productDetails.product.details.specialOffers}</p>
+                            <h3 className='text-center text-danger'>{productDetails.product.name}</h3>
+                            <hr className='my-4' />
+                            <ul className="list-group text-success">
+                                {onShowconfiguration(productDetails.product.details.configuration)}
+                            </ul>
+                            <hr className='my-4' />
+                            <p className='text-primary'>{productDetails.product.details.specialOffers}</p>
                             <hr className='my-4' />
                             <ul className='imageDeatil'>
                                 <li>
@@ -169,7 +196,7 @@ const ProductDetailPage = props => {
                 <div className="container-fluid padding product-detail">
                     <div className="row padding my-2">
                         <div className="col-md-5 text-center">
-                            <a href>
+                            <a href={productDetails.product_hot.images} alt={productDetails.product_hot.name}>
                                 <img className='img-fluid' width='350px' src={productDetails.product_hot.images} alt={productDetails.product_hot.name} />
                             </a>
                             <div>
@@ -182,17 +209,21 @@ const ProductDetailPage = props => {
                                     </li>
                                 </ul>
                                 <p>Giá: {productDetails.product_hot.price} $</p>
-                                <button className='btn btn-outline-primary'><i className='fa fa-shopping-cart'></i> Thêm Vào Giỏ Hàng</button>
+                                <Link className='btn btn-outline-primary' to='/product-cart' onClick={() => onAddToCard(productDetails)}><i className='fa fa-shopping-cart'></i> Thêm Vào Giỏ Hàng</Link>
+                            </div>
+                            <hr className='my-4' />
+                            <div className='card text-left'>
+                                <h5><i className='text-danger'>Thông tin sản phẩm : </i><span className='text-secondary'>{productDetails.product_hot.details.decscription}</span> </h5>
                             </div>
                         </div>
                         <div className="col-md-4">
-                            <h1>{productDetails.product_hot.name}</h1>
-                            <hr className='my-2' />
-                            <p><i>Thông tin sản phẩm : </i>{productDetails.product_hot.details.decscription} </p>
-                            <hr className='my-2' />
-                            <h4>{productDetails.product_hot.details.configuration}</h4>
-                            <hr className='my-2' />
-                            <p>{productDetails.product_hot.details.specialOffers}</p>
+                            <h3 className='text-center text-danger'>{productDetails.product_hot.name}</h3>
+                            <hr className='my-4' />
+                            <ul className="list-group text-success">
+                                {onShowconfiguration(productDetails.product_hot.details.configuration)}
+                            </ul>
+                            <hr className='my-4' />
+                            <p className='text-primary'>{productDetails.product_hot.details.specialOffers}</p>
                             <hr className='my-4' />
                             <ul className='imageDeatil'>
                                 <li>
@@ -222,8 +253,6 @@ const ProductDetailPage = props => {
             </>
         )
     }
-
-
 }
 
 const mapStateToProps = state => {
@@ -234,7 +263,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (dispatch, props) => {
     return {
-
+        add_product: product => {
+            dispatch(Product_Cart_dispatch(product))
+        },
+        changeMessage: message => {
+            dispatch(changeMessage_add_dispatch(message))
+        }
     }
 }
 
