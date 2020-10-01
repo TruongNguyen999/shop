5a480d8755f366105a2d4af39d5c5ba435e2830e
TruongNguyen999 <nguyentruong061999@gmail.com>
2020-10-01 23:23:54 +0700

    admin

--- src/page/Administration/Administration.js
+++ src/page/Administration/Administration.js
@@ -3,7 +3,103 @@ import React from 'react';
 const Administration = props => {
     return (
         <>
-            quan tri
+            <div className='administration'>
+                <div className='menu'>
+                    <h3 className='text-white text-center mt-4'>SHOP PHONE</h3>
+                    <hr className='my-3' />
+                    <ul>
+                        <li>
+                            <a href='#'><i className="fa fa-cart-plus mr-3 " aria-hidden="true"></i> Sản Phẩm</a>
+                        </li>
+                        <hr className='my-1' />
+                        <li>
+                            <a href='#'><i className="fa fa-newspaper-o mr-3 " aria-hidden="true"></i> Tin Tức</a>
+                        </li>
+                        <hr className='my-1' />
+                        <li>
+                            <a href='#'><i className="fa fa-user mr-3 " aria-hidden="true"></i> Người Dùng</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className='content'>
+
+                    <div class="navbar">
+                        <a className="navbar-brand" href="#">Nguyên Nhựt Trường</a>
+                        <ul className="nav navbar-nav">
+                            <li className="active">
+                                <a href="#">tài khoản : Trường    </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='main-content'>
+                        <ul class="list-group">
+                            <li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                        </ul>
+                        <ul class="list-group">
+                            <li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                        </ul>
+                        <ul class="list-group">
+                            <li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                        </ul>
+                        <ul class="list-group">
+                            <li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                        </ul>
+
+                        <ul class="list-group">
+                            <li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                        </ul><ul class="list-group">
+                            <li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                        </ul>
+                        <ul class="list-group">
+                            <li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                        </ul>
+                        <ul class="list-group">
+                            <li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                        </ul>
+                        <ul class="list-group">
+                            <li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                        </ul>
+                        <ul class="list-group">
+                            <li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                        </ul>
+                        <ul class="list-group">
+                            <li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                        </ul>
+                        <ul class="list-group">
+                            <li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                        </ul><ul class="list-group">
+                            <li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                        </ul>
+                        
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
